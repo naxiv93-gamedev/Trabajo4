e70a07bc74c261663bd292bebeeaef6fb6d80f80
naxiv93-gamedev <72494774+naxiv93-gamedev@users.noreply.github.com>
2022-06-19 21:08:44 +0200

    tags

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+ 
+setup(
+    name='AngleDisplay',
+    packages=['Trabajo4'], # Mismo nombre que en la estructura de carpetas de arriba
+    version='0.1',
+    license='MIT', # La licencia que tenga tu paquete
+    description='Display de angulo',
+    author='naxiv93-gamedev',
+    author_email='ivandiaz93@gmail.com',
+    url='https://github.com/naxiv93-gamedev/Trabajo4', # Usa la URL del repositorio de GitHub
+    download_url='https://github.com/naxiv93-gamedev/Trabajo4/archive/v0.1.tar.gz.', # Te lo explico a continuación
+    keywords='test example develop', # Palabras que definan tu paquete
+    classifiers=['Programming Language :: Python',  # Clasificadores de compatibilidad con versiones de Python para tu paquete
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3.3',
+                 'Programming Language :: Python :: 3.4',
+                 'Programming Language :: Python :: 3.5',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7'],
+)
